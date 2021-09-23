a107e0909c79bb42a3782caf830cd2bf6beab057
SunilSuryawanshi <07sraje@gmail.com>
2021-09-23 23:13:10 +0530

    sidebar
    sidebar completed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,36 +6,42 @@ import github from '../assets/icons/github.svg';
 import pin from '../assets/icons/pin.svg';
 import tie from '../assets/icons/tie.svg';
 import mightycoder from '../assets/mightycoder.svg';
-
+import resume from '../assets/resume.pdf';
 
 const Sidebar = () => {
+    const handleEmailMe = () => {
+        window.open("mailto:sunilsuryawanshi939@gmail.com")
+    }
     return (
         <div className="sidebar">
             <img src={mightycoder} alt="avatar" className="sidebar__avatar"/>
             <div className="sidebar__name">Sunil <span>Suryawanshi</span></div>
             <div className="sidebar__item sidebar__title"> Web Developer</div>
-            <a href="!#">
-                <div className="sidebar__item">
+            <a href={resume} download="resume.pdf">
+                <div className="sidebar__item sidebar__resume">
                     <img src={tie} alt="Resume" className="sidebar__icon"/> Download Resume
                 </div>
             </a>
-            <figure className="sidebar__social-icons">
-                <a href=""><img src={facebook} alt="facebook" className="sidebar__icon" />facebook</a>
-                <a href=""><img src={instagram} alt="instagram" className="sidebar__icon" />instagram</a>
+            <figure className="sidebar__social-icons my-2">
+                <a href=""><img src={facebook} alt="facebook" className="sidebar__icon mr-3" /></a>
+                <a href=""><img src={instagram} alt="instagram" className="sidebar__icon" /></a>
             </figure>
-            <div className="contact">
-                <div className="sidebar__item">
-                <a href=""><img src={github} alt="github" className="sidebar__icon" />github</a>
+            <div className="sidebar__contact">
+                <div className="sidebar__item sidebar__github">
+                <a href=""><img src={github} alt="github" className="sidebar__icon mr-3" />github</a>
                 </div>
                 <div className="sidebar__location">
-                    <img src={pin} alt="location" className="sidebar__icon" />
-                    Sangli,India</div>
-                <div className="sidebar__item">code.sunilsuryawanshi939@gmail.com</div>
+                <img src={pin} alt="location" className="sidebar__icon mr-3" />Sangli,India</div>
+                <div className="sidebar__item email">sunilsuryawanshi939@gmail.com</div>
                 <div className="sidebar__item">8530796939</div>
-                <div className="sidebar__item sidebar__email">Email me</div>
             </div>
+            <div className="sidebar__item sidebar__email" onClick={handleEmailMe}>Email me</div>
         </div>
     )
 }
 
 export default Sidebar
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
             use: ["style-loader", "css-loader"],
         },
         {
-         test: /\.(png|jpe?g|gif)$/i,
+         test: /\.(pdf|png|jpe?g|gif)$/i,
          loader: 'file-loader',
          options: {
          outputPath: 'images',
